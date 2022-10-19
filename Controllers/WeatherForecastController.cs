4a826da6e8655c49a0e489e8b46b095254653030
harikrishnan.a@psiog.com <harikrishnan.a@psiog.com>
2022-10-19 18:17:49 +0530

    implement feature 1

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace TestWebAPI.Controllers
   {
     private static readonly string[] Summaries = new[]
     {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Heatwave"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Heatwave", "feature1"
         };
 
     private readonly ILogger<WeatherForecastController> _logger;
